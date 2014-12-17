d42147165a6dc282e3a8437bbe9a7d3659705eda
Doug Stoeckmann <dousto@gmail.com>
2014-12-17 06:31:53 +0000

    Adding logic files, and with a sample of the rhythm logic at /users

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var asp = require('asp');
+var path = require('path');
+var Clingo = require('clingojs');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('ASP Version: ' + asp.version);
+    var clingo = new Clingo().config({
+        maxModels: 0
+    });
+
+    var models = new Array();
+
+    clingo.solve({
+        inputFiles: path.resolve(__dirname, '../lp/rhythm'),
+        constants: { from: 0, to: 1}
+    })
+        .on('model', function(model) {
+            models.push(model);
+        })
+        .on('end', function() {
+            res.type('json');
+            res.send(JSON.stringify(models));
+        })
 });
 
 module.exports = router;
