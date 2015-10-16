23634a5845336197f1b7a191e0f51f905d3d2d61
Doug Stoeckmann <dousto@gmail.com>
2015-10-15 23:37:57 -0700

    Adding ability to customize the rng used by song generator

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var redact = require('./routes/redact');
+var redact_face = require('./routes/face');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/redact', redact);
+app.use('/redact-face', redact_face);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
